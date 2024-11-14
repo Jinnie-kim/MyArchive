bbf488b34333934866f5cd9862607c0a3688fc79
Jinnie <hjk7639@gmail.com>
2024-11-14 19:26:46 +0900

    Add: Home Page Component

---
+++ src/page/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="w-full min-w-[1000px] h-full flex flex-col items-center gap-[20px]">
+      <header>
+        <h1 className="text-4xl font-bold">⊹⁺⸜ My Archive ⸝⁺⊹</h1>
+      </header>
+      <main className="relative w-[850px] max-w-[90vw] h-[600px] bg-main-rgba rounded-[20px] p-[20px]"></main>
+    </div>
+  );
+}
+
+export default Home;
