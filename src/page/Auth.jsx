4506f132d84e1b570de73753c52dc7a09b6fe1dd
Jinnie <hjk7639@gmail.com>
2024-11-15 16:26:46 +0900

    Style: remove background colour from div and button text styles

--- src/page/Auth.jsx
+++ src/page/Auth.jsx
@@ -4,15 +4,15 @@ function Auth() {
       <header>
         <h1 className="text-4xl font-bold">⊹⁺⸜ My Archive ⸝⁺⊹</h1>
       </header>
-      <main className="relative w-[850px] max-w-[90vw] h-[600px] bg-main-rgba rounded-[20px] p-[20px]">
+      <main className="relative w-[850px] max-w-[90vw] h-[600px] p-[20px]">
         <div className="flex items-center justify-center h-full gap-[100px]">
           <div>
-            <button type="button" className="w-[150px] p-[10px] border-b border-[#fec2d6] hover:text-[#fec2d6]">
+            <button type="button" className="w-[150px] p-[10px] border-b border-[#fec2d6] hover:text-[#fec2d6] text-white">
               Login
             </button>
           </div>
           <div>
-            <button type="button" className="w-[150px] p-[10px] border-b border-[#bd87fa] hover:text-[#bd87fa]">
+            <button type="button" className="w-[150px] p-[10px] border-b border-[#bd87fa] hover:text-[#bd87fa] text-white">
               Submit
             </button>
           </div>
