0c105f377414445550d9606d6d2caaa428599a4f
Jinnie <hjk7639@gmail.com>
2024-11-17 16:32:40 +0900

    redesign: update Home page layout and section

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,47 @@
+import ArchiveCard from '../components/ArchiveCard';
+import ArchiveTypeSectionLayout from '../layout/ArchiveTypeSectionLayout';
 import GlobalLayout from '../layout/GlobalLayout';
 
 function Home() {
-  return <GlobalLayout backgroundColor="bg-main-rgba" />;
+  return (
+    <GlobalLayout>
+      <ArchiveTypeSectionLayout isAuthenticated={true}>
+        <section className="flex flex-col gap-12">
+          <a href="/" className="ml-auto pb-[5px] border-b-[2px] border-[#DCABAB] hover:text-[#DCABAB]">
+            See all archives
+          </a>
+          <div>
+            <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-rows-2 gap-12">
+              <li>
+                <ArchiveCard />
+              </li>
+              <li>
+                <ArchiveCard />
+              </li>
+              <li>
+                <ArchiveCard />
+              </li>
+              <li>
+                <ArchiveCard />
+              </li>
+              <li>
+                <ArchiveCard />
+              </li>
+              <li>
+                <ArchiveCard />
+              </li>
+              <li>
+                <ArchiveCard />
+              </li>
+              <li>
+                <ArchiveCard />
+              </li>
+            </ul>
+          </div>
+        </section>
+      </ArchiveTypeSectionLayout>
+    </GlobalLayout>
+  );
 }
 
 export default Home;
