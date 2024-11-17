143af16f6cdf0405e816c1566d7958f77eceebc7
Jinnie <hjk7639@gmail.com>
2024-11-17 15:11:21 +0900

    redesign: update Signup page with new minimalist style

--- src/page/Signup.jsx
+++ src/page/Signup.jsx
@@ -3,31 +3,35 @@ import GlobalLayout from '../layout/GlobalLayout';
 function Signup() {
   return (
     <GlobalLayout>
-      <div className="h-full flex items-center justify-center">
-        <div className="relative flex flex-col gap-[20px] w-[400px] max-[600px] bg-main-rgba rounded-[20px] p-[20px]">
-          <h1 className="text-[#ed7790] text-center font-bold text-[24px]">SING UP</h1>
-          <form className="flex flex-col gap-[20px]">
-            <div>
-              <label htmlFor="email" className="text-[#ed7790] font-semibold">
-                Email
-              </label>
-              <input type="email" id="email" className="w-full p-[15px] rounded-[10px] bg-transparent border border-[#ff6072] text-[#ed7790] focus:outline-0" />
-            </div>
-            <div>
-              <label htmlFor="password" className="text-[#ed7790] font-semibold">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                className="w-full p-[15px] rounded-[10px] bg-transparent border border-[#ff6072]  text-[#ed7790] focus:outline-0"
-              />
+      <div className="w-full h-full flex items-center justify-center">
+        <div className="w-[600px] p-[50px] flex flex-col gap-[50px] border border-[#CE8181] rounded-[10px]">
+          <h2 className="text-[24px]">Sign Up</h2>
+
+          <form className="flex flex-col gap-[30px]">
+            <div className="flex flex-col gap-[10px]">
+              <label htmlFor="email">Email</label>
+              <input type="email" className="p-[10px] rounded-[10px] border border-[#E8D7D7] focus:outline-[#DCABAB] bg-transparent" />
             </div>
 
-            <button type="submit" className="w-full p-[15px] rounded-[10px] bg-[#ff7a85] text-white font-semibold cursor-pointer hover:bg-[#ff6072]">
-              Sing up
+            <div className="flex flex-col gap-[10px]">
+              <label htmlFor="password">Password</label>
+              <input type="password" className="p-[10px] rounded-[10px] border border-[#E8D7D7] focus:outline-[#DCABAB] bg-transparent" />
+              <span className="text-[12px]">Password must be at least 8 characters</span>
+            </div>
+
+            <div className="flex flex-col gap-[10px]">
+              <label htmlFor="confirm_password">Confirm Password</label>
+              <input type="password" className="p-[10px] rounded-[10px] border border-[#E8D7D7] focus:outline-[#DCABAB] bg-transparent" />
+            </div>
+
+            <button type="submit" className="p-[10px] rounded-[10px] bg-[#E8D7D7]">
+              Create Account
             </button>
           </form>
+
+          <span className="mx-auto">
+            Already have an account? <a className="text-[#DCABAB] hover:text-[#CE8181] cursor-pointer">Log in</a>
+          </span>
         </div>
       </div>
     </GlobalLayout>
