31c544d073a0ffc5b74f6271342ddb40cda61eeb
Jinnie <hjk7639@gmail.com>
2024-11-17 14:26:30 +0900

    redesign: update login page with new minimalist style

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -3,32 +3,33 @@ import GlobalLayout from '../layout/GlobalLayout';
 function Login() {
   return (
     <GlobalLayout>
-      <div className="h-full flex items-center justify-center">
-        <div className="relative flex flex-col gap-[20px] w-[400px] max-[600px] bg-main-rgba rounded-[20px] p-[20px]">
-          <h1 className="text-[#ed7790] text-center font-bold text-[24px]">LOGIN</h1>
-          <form className="flex flex-col gap-[20px]">
-            <div>
-              <label htmlFor="email" className="text-[#ed7790] font-semibold">
-                Email
-              </label>
-              <input type="email" id="email" className="w-full p-[15px] rounded-[10px] bg-transparent border border-[#ff6072] text-[#ed7790] focus:outline-0" />
-            </div>
-            <div>
-              <label htmlFor="password" className="text-[#ed7790] font-semibold">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                className="w-full p-[15px] rounded-[10px] bg-transparent border border-[#ff6072]  text-[#ed7790] focus:outline-0"
-              />
+      <div className="w-full h-full flex items-center justify-center">
+        <div className="w-[600px] p-[50px] flex flex-col gap-[50px] border border-[#CE8181] rounded-[10px]">
+          <h2 className="text-[24px]">Login</h2>
+
+          <form className="flex flex-col gap-[30px]">
+            <div className="flex flex-col gap-[10px]">
+              <label htmlFor="email">Email</label>
+              <input type="email" className="p-[10px] rounded-[10px] border border-[#E8D7D7] focus:outline-[#DCABAB]" />
             </div>
 
-            <button type="submit" className="w-full p-[15px] rounded-[10px] bg-[#ff7a85] text-white font-semibold cursor-pointer hover:bg-[#ff6072]">
+            <div className="flex flex-col gap-[10px]">
+              <label htmlFor="password">Password</label>
+              <input type="password" className="p-[10px] rounded-[10px] border border-[#E8D7D7] focus:outline-[#DCABAB]" />
+
+              <a href="/" className="ml-auto text-[14px] hover:text-[#CE8181]">
+                Forgot Password?
+              </a>
+            </div>
+
+            <button type="submit" className="p-[10px] rounded-[10px] bg-[#E8D7D7] hover:bg-[#DCABAB]">
               Login
             </button>
           </form>
-          <a className="self-end text-white text-[15px] pb-[5px] hover:text-[#ff6072] cursor-pointer">go to signup</a>
+
+          <span className="mx-auto">
+            Don&#39;t you have an account? <a className="text-[#DCABAB] hover:text-[#CE8181] cursor-pointer">Sign up</a>
+          </span>
         </div>
       </div>
     </GlobalLayout>
