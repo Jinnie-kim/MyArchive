858ed965ffbeac0751449a5e1e02200f6e2a82df
Jinnie <hjk7639@gmail.com>
2024-11-25 17:10:01 +0900

    add: Implement signup form validation with password confirmation

--- src/page/Signup.jsx
+++ src/page/Signup.jsx
@@ -4,19 +4,39 @@ import { useState } from 'react';
 
 function Signup() {
   const [email, setEmail] = useState('');
+  const [emailValidation, setEmailValidation] = useState(false);
   const [password, setPassword] = useState('');
+  const [passwordValidation, setPasswordValidation] = useState(false);
   const [confirmPassword, setConfirmPassowrd] = useState('');
+  const [confirmPasswordValidation, setConfirmPasswordValidation] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const emailRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+  const passwordRegex = /^(?=.*[a-z])(?=.*[0-9]).{8,}$/;
 
   const getUserEmail = (e) => {
     setEmail(e.target.value);
+    setEmailValidation(!emailRegex.test(email));
   };
 
   const getUserPassword = (e) => {
     setPassword(e.target.value);
+    setPasswordValidation(!passwordRegex.test(password));
   };
 
   const getConfirmPassowrd = (e) => {
     setConfirmPassowrd(e.target.value);
+
+    if (password && e.target.value !== password) {
+      setConfirmPasswordValidation(true);
+    } else {
+      setConfirmPasswordValidation(false);
+      makeReadytoSignup();
+    }
+  };
+
+  const makeReadytoSignup = () => {
+    setIsDisabled(!emailValidation && !passwordValidation && !confirmPasswordValidation);
   };
 
   return (
@@ -27,7 +47,10 @@ function Signup() {
 
           <form className="flex flex-col gap-[30px]">
             <div className="flex flex-col gap-[10px]">
-              <label htmlFor="email">Email</label>
+              <div className="flex items-center gap-1">
+                <label htmlFor="email">Email</label>
+                {emailValidation && <span className="self-end text-[12px] text-[#CE8181]">이메일은 최소한 @와 .를 포함해야합니다.</span>}
+              </div>
               <input
                 type="email"
                 value={email}
@@ -37,18 +60,23 @@ function Signup() {
             </div>
 
             <div className="flex flex-col gap-[10px]">
-              <label htmlFor="password">Password</label>
+              <div className="flex items-center gap-1">
+                <label htmlFor="password">Password</label>
+                {passwordValidation && <span className="self-end text-[12px] text-[#CE8181]">비밀번호는 영어, 숫자 포함 최소 8자 이상이어야 합니다.</span>}
+              </div>
               <input
                 type="password"
                 value={password}
                 onChange={getUserPassword}
                 className="p-[10px] rounded-[10px] border border-[#E8D7D7] focus:outline-[#DCABAB] bg-transparent"
               />
-              <span className="text-[12px]">Password must be at least 8 characters</span>
             </div>
 
             <div className="flex flex-col gap-[10px]">
-              <label htmlFor="confirm_password">Confirm Password</label>
+              <div className="flex items-center gap-1">
+                <label htmlFor="confirm_password">Confirm Password</label>
+                {confirmPasswordValidation && <span className="self-end text-[12px] text-[#CE8181]">입력한 비밀번호와 일치하지 않습니다.</span>}
+              </div>
               <input
                 type="password"
                 value={confirmPassword}
@@ -57,7 +85,11 @@ function Signup() {
               />
             </div>
 
-            <button type="submit" className="p-[10px] rounded-[10px] bg-[#E8D7D7]">
+            <button
+              type="submit"
+              disabled={isDisabled}
+              className={`p-[10px] rounded-[10px] ${isDisabled ? 'bg-[#E8D7D7] cursor-not-allowed' : 'cursor-pointer bg-[#DCABAB]'}`}
+            >
               Create Account
             </button>
           </form>
